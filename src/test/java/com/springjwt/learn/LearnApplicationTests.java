affa3a6cc3731487c8fc6fa23d961115202b448f
kym <ymyj33@naver.com>
2022-04-05 17:18:38 +0900

    [feat] jwt first commit

---
+++ src/test/java/com/springjwt/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjwt.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
